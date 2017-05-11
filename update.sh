8c9ecc441413aa1e25c40498982dc554addbf579
Kuvakin Sergey <kuvakin@gmail.com>
2017-05-12 00:07:41 +0300

    refactoring

--- update.sh
+++ update.sh
@@ -1,116 +1,84 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
-cd "$(readlink -f "$(dirname "$BASH_SOURCE")")"
+# A POSIX variable
+OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
-versions=( "$@" )
-if [ ${#versions[@]} -eq 0 ]; then
-	versions=( */ )
-fi
-versions=( "${versions[@]%/}" )
-
-get_part() {
-	dir="$1"
-	shift
-	part="$1"
-	shift
-	if [ -f "$dir/$part" ]; then
-		cat "$dir/$part"
-		return 0
-	fi
-	if [ -f "$part" ]; then
-		cat "$part"
-		return 0
-	fi
-	if [ $# -gt 0 ]; then
-		echo "$1"
-		return 0
-	fi
-	return 1
-}
-
-latest="$(get_part . latest '')"
-
-repo="$(get_part . repo '')"
-if [ "$repo" ]; then
-	if [[ "$repo" != */* ]]; then
-		user="$(docker info | awk '/^Username:/ { print $2 }')"
-		if [ "$user" ]; then
-			repo="$user/$repo"
-		fi
-	fi
-fi
-
-for version in "${versions[@]}"; do
-	dir="$(readlink -f "$version")"
-	variant="$(get_part "$dir" variant 'minbase')"
-	components="$(get_part "$dir" components 'main')"
-	include="$(get_part "$dir" include '')"
-	suite="$(get_part "$dir" suite "$version")"
-	mirror="$(get_part "$dir" mirror '')"
-	script="$(get_part "$dir" script '')"
-	arch="$(get_part "$dir" arch '')"
-	qemu_arch="$(get_part "$dir" qemu_arch $arch)"
-	uname_arch="$(get_part "$dir" uname_arch $arch)"
-	#debootstrap="$(get_part "$dir" debootstrap 'debootstrap')"
-
-	args=( -d "$dir" debootstrap )
-	[ -z "$variant" ] || args+=( --variant="$variant" )
-	[ -z "$components" ] || args+=( --components="$components" )
-	[ -z "$include" ] || args+=( --include="$include" )
-	[ -z "$arch" ] || args+=( --arch="$arch" )
-	args+=( "$suite" )
-	if [ "$mirror" ]; then
-		args+=( "$mirror" )
-		if [ "$script" ]; then
-			args+=( "$script" )
-		fi
-	fi
-
-	if [ "$SKIP_DEBOOTSTRAP" != "1" ]; then
-	    mkimage="$(readlink -f "${MKIMAGE:-"mkimage.sh"}")"
-	    {
-		echo "$(basename "$mkimage") ${args[*]/"$dir"/.}"
-		echo
-		echo 'https://github.com/docker/docker/blob/master/contrib/mkimage.sh'
-	    } > "$dir/build-command.txt"
-	    
-	    sudo DEBOOTSTRAP="qemu-debootstrap" nice ionice -c 3 "$mkimage" "${args[@]}" 2>&1 | tee "$dir/build.log"
-	    
-	    sudo chown -R "$(id -u):$(id -g)" "$dir"
-
-            xz -d < $dir/rootfs.tar.xz | gzip -c > $dir/rootfs.tar.gz
-	fi
-
-	
-	if [ "$repo" ]; then
-	    sed -i /^ENV/d "${dir}/Dockerfile"
-	    echo "ENV ARCH=${uname_arch} UBUNTU_SUITE=${suite} DOCKER_REPO=${repo}" >> "${dir}/Dockerfile"
-	    docker build -t "${repo}:${arch}-${suite}-slim" "${dir}"
-	    mkdir -p "${dir}/full"
-	    cat > "${dir}/full/Dockerfile" <<EOF
-FROM ${repo}:${arch}-${suite}-slim
-ADD https://github.com/multiarch/qemu-user-static/releases/download/v2.7.0/amd64_qemu-${qemu_arch}-static.tar.gz /usr/bin
-EOF
-	    docker build -t "${repo}:${arch}-${suite}" "${dir}/full"
-	fi
-	
-	if [ "${latest}" = "${suite}" ]; then
-	    docker tag -f "${repo}:${arch}-${suite}-slim" "${repo}:${arch}-slim"
-	    docker tag -f "${repo}:${arch}-${suite}" "${repo}:${arch}"
-	fi
-
-	docker run -it --rm "${repo}:${arch}-${suite}" bash -xc '
-                        uname -a
-                        echo
-			cat /etc/apt/sources.list
-			echo
-			cat /etc/os-release 2>/dev/null
-			echo
-			cat /etc/lsb-release 2>/dev/null
-			echo
-			cat /etc/debian_version 2>/dev/null
-			true
-		'
+while getopts "a:v:q:u:d:s:i:o:" opt; do
+    case "$opt" in
+    a)  ARCH=$OPTARG
+        ;;
+    v)  VERSION=$OPTARG
+        ;;
+    q)  QEMU_ARCH=$OPTARG
+        ;;
+    u)  QEMU_VER=$OPTARG
+        ;;
+    d)  DOCKER_REPO=$OPTARG
+        ;;
+    s)  SUITE=$OPTARG
+        ;;
+    i)  INCLUDE=$OPTARG
+        ;;
+    o)  UNAME_ARCH=$OPTARG
+        ;;
+    esac
 done
 
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+dir="$VERSION"
+COMPONENTS="main"
+VARIANT="minbase"
+args=( -d "$dir" debootstrap --no-check-gpg --variant="$VARIANT" --components="$COMPONENTS" --include="$INCLUDE" --arch="$ARCH" "$SUITE" )
+
+mkdir -p mkimage $dir
+curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage.sh > mkimage.sh
+curl https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage/debootstrap > mkimage/debootstrap
+chmod +x mkimage.sh mkimage/debootstrap
+
+mkimage="$(readlink -f "${MKIMAGE:-"mkimage.sh"}")"
+{
+    echo "$(basename "$mkimage") ${args[*]/"$dir"/.}"
+    echo
+    echo 'https://github.com/docker/docker/blob/master/contrib/mkimage.sh'
+} > "$dir/build-command.txt"
+
+sudo DEBOOTSTRAP="qemu-debootstrap" nice ionice -c 3 "$mkimage" "${args[@]}" 2>&1 | tee "$dir/build.log"
+cat "$dir/build.log"
+sudo chown -R "$(id -u):$(id -g)" "$dir"
+
+xz -d < $dir/rootfs.tar.xz | gzip -c > $dir/rootfs.tar.gz
+sed -i /^ENV/d "${dir}/Dockerfile"
+echo "ENV ARCH=${UNAME_ARCH} UBUNTU_SUITE=${SUITE} DOCKER_REPO=${DOCKER_REPO}" >> "${dir}/Dockerfile"
+
+if [ "$DOCKER_REPO" ]; then
+    docker build -t "${DOCKER_REPO}:${ARCH}-${SUITE}-slim" "${dir}"
+    mkdir -p "${dir}/full"
+    (
+    cd "${dir}/full"
+    if [ ! -f x86_64_qemu-${QEMU_ARCH}-static.tar.gz ]; then
+        wget -N https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VER}/x86_64_qemu-${QEMU_ARCH}-static.tar.gz
+    fi
+    tar xf x86_64_qemu-*.gz
+    )
+    cat > "${dir}/full/Dockerfile" <<EOF
+FROM ${DOCKER_REPO}:${ARCH}-${SUITE}-slim
+ADD qemu-*-static /usr/bin/
+EOF
+    docker build -t "${DOCKER_REPO}:${ARCH}-${SUITE}" "${dir}/full"
+fi
+
+docker run -it --rm "${DOCKER_REPO}:${ARCH}-${SUITE}" bash -xc '
+    uname -a
+    echo
+    cat /etc/apt/sources.list
+    echo
+    cat /etc/os-release 2>/dev/null
+    echo
+    cat /etc/lsb-release 2>/dev/null
+    echo
+    cat /etc/debian_version 2>/dev/null
+    true
+'
